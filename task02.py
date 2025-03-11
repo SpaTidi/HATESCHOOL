e211e072a13ed5bc550b1727e56dc99271ff5c0d
SpaTidi <voskoboynikov_vlad@mail.ru>
2025-03-11 19:59:22 +1000

    final_done

--- task02.py
+++ task02.py
@@ -9,18 +9,23 @@ We guarantee, that the given sequence contain >= 0 integers inside.
 from typing import Sequence
 
 def check_fibonacci(data: Sequence[int]) -> bool:
-    if len(data) < 2:
-        return True # Думаю лучше тут сделать True. 1 или 2 элемента по умолчанию являются Фибоначчи, так как нет третьего.
+        if len(data) == 1:
+                return data[0] == 0 
     
-    for i in range(2, len(data)):
-        if data[i] != data[i - 1] + data[i - 2]:
-            return False
+        if len(data) == 2:
+                return data[0] == 0 and data[1] == 1
+        
+        if data[0] != 0 or data[1] != 1:
+                return False
     
-    return True
+        for i in range(2, len(data)):
+            if data[i] != data[i - 1] + data[i - 2]:
+                    return False
+    
+        return True
 
 if __name__ == '__main__':
+        print(check_fibonacci([0, 1, 1]))
         print(check_fibonacci([1, 1, 2]))
-        print(check_fibonacci([1, 2, 2]))
-        print(check_fibonacci([1]))
-        print(check_fibonacci([1, 1, 1]))
-        print(check_fibonacci([3, 5, 8]))
+        print(check_fibonacci([0]))
+        print(check_fibonacci([0, 1]))
